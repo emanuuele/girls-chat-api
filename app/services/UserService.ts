9e16cef88eaed4cd6710938d3f1bb6035c1f6a75
Emanuele Maria <emanuuuelem@gmail.com>
2025-11-18 03:11:26 -0300

    fix: add logging for user profile picture upload process in saveProfilePicture method

--- app/services/UserService.ts
+++ app/services/UserService.ts
@@ -101,16 +101,20 @@ export default class UserService {
                 throw new Error("Usuário não encontrado");
             }
             user.profile_picture = `${userID}.${file.extname}`;
+            console.log("user.profile_picture", user);
             const metadata = await this.saveProfilePictureGoogleStorage(file, user.profile_picture);
             if (!metadata || !metadata.publicUrl()) {
                 console.log("chegou aqui");
                 throw new Error("Falha ao fazer upload da imagem");
             }
+            console.log("user.profile_picture", user);            
             await metadata.makePublic()
             user.profile_picture = metadata.publicUrl();
             await user.save();
             return user;
         } catch (error) {
+            console.log('qqaaa');
+            
             throw new Error("Erro ao salvar imagem de perfil: " + error.message);
         }
     }
