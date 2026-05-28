dd81efb2d8eec50bfd070d323962168f782d7259
Emanuele Maria <emanuuuelem@gmail.com>
2026-05-28 07:46:47 -0300

    fix: update CORS configuration to allow all origins in IoSocketServer

--- app/services/IoSocketServer.ts
+++ app/services/IoSocketServer.ts
@@ -15,10 +15,7 @@ class Ws extends SocketIoServer {
     this.io = new SocketIoServer(Server.instance!, {
       cors: {
         origin: [
-          "http://localhost:8081", 
-          "http://localhost:3000", 
-          "http://127.0.0.1:8081",
-          "http://127.0.0.1:3000"
+          "*", 
         ],
         methods: ["GET", "POST"],
         credentials: true
