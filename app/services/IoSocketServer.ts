71371f197a860dccad781403ad0e54e021a970c7
Emanuele Maria <emanuuuelem@gmail.com>
2026-05-28 08:18:46 -0300

    fix: update CORS configuration to specify allowed origins in IoSocketServer

--- app/services/IoSocketServer.ts
+++ app/services/IoSocketServer.ts
@@ -15,7 +15,12 @@ class Ws extends SocketIoServer {
     this.io = new SocketIoServer(Server.instance!, {
       cors: {
         origin: [
-          "*", 
+          "http://localhost:8081", 
+          "http://localhost:3000", 
+          "http://127.0.0.1:8081",
+          "http://127.0.0.1:3000",
+          "https://portfolio-three-sigma-3mfoqj1rmx.vercel.app",
+          "*",
         ],
         methods: ["GET", "POST"],
         credentials: true
